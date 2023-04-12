348fdae9bd562414607398eb2b7200b7ff6ecbc8
lsduser <pleasespamthisaddy5@gmail.com>
2023-04-12 16:15:42 +0100

    navbar animation

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -9,13 +9,11 @@ import clsx from "clsx";
 type Props = {};
 
 const Navbar = (props: Props) => {
-  const navbarRef = useRef<HTMLElement>(null);
-  const openMenuRef = useRef<HTMLDivElement>(null);
-  const closeMenuRef = useRef<HTMLDivElement>(null);
-  const navMenuRef = useRef<HTMLDivElement>(null);
+  const navListRef = useRef<HTMLUListElement>(null);
   const blurOverlayRef = useRef<HTMLDivElement>(null);
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [visible, setVisible] = useState(true);
+  const [showNavLinks, setShowNavLinks] = useState(false);
 
   const handleScroll = () => {
     const currentScrollPos = window.scrollY;
@@ -36,15 +34,14 @@ const Navbar = (props: Props) => {
   });
 
   const onOpenCloseClick = () => {
-    if (navMenuRef.current && openMenuRef.current && blurOverlayRef.current) {
-      openMenuRef.current.dataset.show =
-        openMenuRef.current.dataset.show === "false" ? "true" : "false";
-      navMenuRef.current.dataset.show =
-        navMenuRef.current.dataset.show === "false" ? "true" : "false";
+    setShowNavLinks(!showNavLinks);
+    if (blurOverlayRef.current && navListRef.current) {
       blurOverlayRef.current.dataset.show =
         blurOverlayRef.current.dataset.show === "false" ? "true" : "false";
-      document.getElementsByTagName("body")[0].style.overflow =
-        navMenuRef.current.dataset.show === "false" ? "" : "hidden";
+      navListRef.current.dataset.open =
+        navListRef.current.dataset.open === "false" ? "true" : "false";
+      document.body.style.overflow =
+        document.body.style.overflow === "hidden" ? "" : "hidden";
     }
   };
 
@@ -52,37 +49,40 @@ const Navbar = (props: Props) => {
     <>
       <nav
         className={clsx(
-          "z-10 w-full backdrop-blur flex-none transition-all duration-[600ms] lg:z-40 bg-secondary/white supports-backdrop-blur:bg-secondary-white/60 dark:bg-transparent flex justify-between items-center px-4 md:px-8 lg:px-[6.25rem] py-4 md:py-6 sticky",
+          "z-20 w-full backdrop-blur flex-none transition-all duration-[600ms] lg:z-40 bg-secondary/white supports-backdrop-blur:bg-secondary-white/60 dark:bg-transparent flex justify-between items-center px-4 md:px-8 lg:px-[6.25rem] py-4 md:py-6 sticky",
           (visible && "top-0") || "-top-[100%]"
         )}
-        ref={navbarRef}
       >
         {/* logo */}
         <Image src={Logo} alt="logo" className="md:w-28 md:h-auto lg:w-14" />
 
         {/* links */}
-        <ul className="hidden lg:flex lg:items-center gap-10 font-medium text-base">
-          <li>
+        <ul
+          className=" max-h-0 lg:max-h-fit absolute lg:relative top-[100%] lg:top-0 left-0 bg-secondary/white w-full lg:w-auto flex flex-col lg:flex-row items-center px-4 md:px-8 lg:px-0 py-1 md:py-2 lg:py-0  gap-2 md:gap-4 lg:gap-10 font-medium text-5xl md:text-6xl lg:text-base overflow-hidden transition-all duration-[600ms] ease-in-out data-[open=true]:h-auto data-[open=true]:max-h-screen"
+          data-open="false"
+          ref={navListRef}
+        >
+          <li className="w-full lg:w-auto text-center py-4 md:py-8 lg:py-0 border-b lg:border-0 border-primary/dark-green/30">
             <a href="#about" className="text-primary/dark-green">
               About
             </a>
           </li>
-          <li>
+          <li className="w-full lg:w-auto text-center py-4 md:py-8 lg:py-0 border-b lg:border-0 border-primary/dark-green/30">
             <a href="#stories" className="text-primary/dark-green">
               Stories
             </a>
           </li>
-          <li>
+          <li className="w-full lg:w-auto text-center py-4 md:py-8 lg:py-0 border-b lg:border-0 border-primary/dark-green/30">
             <a href="#blog" className="text-primary/dark-green">
               Blog
             </a>
           </li>
-          <li>
+          <li className="w-full lg:w-auto text-center py-4 md:py-8 lg:py-0 border-b lg:border-0 border-primary/dark-green/30">
             <a href="#faq" className="text-primary/dark-green">
               FAQ
             </a>
           </li>
-          <li>
+          <li className="w-full lg:w-auto text-center py-4 md:py-8 lg:py-0">
             <a href="#contacts" className="text-primary/dark-green">
               Contacts
             </a>
@@ -96,103 +96,37 @@ const Navbar = (props: Props) => {
         <div
           aria-label="show menu"
           onClick={() => onOpenCloseClick()}
-          ref={openMenuRef}
-          className="data-[show=true]:hidden lg:!hidden"
-          data-show="false"
+          className="lg:!hidden relative flex flex-col h-7 md:h-11 justify-between"
         >
-          <div className="relative w-7 h-1 bg-[#000000] before:content-[''] after:content-[''] before:bg-[#000000] after:bg-[#000000] before:w-7 after:w-7 before:h-1 after:h-1 before:absolute after:absolute before:-top-2 after:-bottom-2"></div>
+          <div
+            className={clsx(
+              "relative  w-6 md:w-10 h-1 bg-[#000000] transition-all duration-[600ms] ease-in-out",
+              showNavLinks && "rotate-45 origin-left"
+            )}
+          ></div>
+          <div
+            className={clsx(
+              "relative  w-6 md:w-10 h-1 bg-[#000000] transition-all duration-[600ms] ease-in-out",
+              showNavLinks &&
+                "-rotate-45 w-8 md:w-14 -translate-x-1 md:-translate-x-2"
+            )}
+          ></div>
+          <div
+            className={clsx(
+              "relative  w-6 md:w-10 h-1 bg-[#000000] transition-all duration-[600ms] ease-in-out",
+              showNavLinks && "rotate-45 origin-right"
+            )}
+          ></div>
         </div>
       </nav>
 
       {/* overlay  */}
       <div
-        className="fixed h-screen w-screen bg-primary/dark-green/70 backdrop-blur-sm z-20 data-[show=false]:hidden overflow-hidden lg:!hidden"
+        className="fixed top-0 h-0 w-screen bg-secondary/sage/30 backdrop-blur-sm z-10 overflow-hidden transition-all duration-[600ms] ease-in-out data-[show=true]:h-screen lg:!hidden"
         ref={blurOverlayRef}
         data-show="false"
         onClick={() => onOpenCloseClick()}
       ></div>
-
-      {/* nav */}
-      <div
-        className="fixed top-4 right-4 w-full max-w-xs bg-primary/acquamarine rounded-3xl shadow-lg p-6 md:p-10 text-base font-semibold text-slate-900 bg-primary-100 flex flex-col gap-3  md:gap-6 z-40 data-[show=false]:hidden lg:!hidden"
-        data-show="false"
-        ref={navMenuRef}
-      >
-        {/* close menu icon  */}
-        <div
-          className="self-end"
-          onClick={() => onOpenCloseClick()}
-          ref={closeMenuRef}
-          aria-label="close menu"
-        >
-          <div
-            className={clsx(
-              "relative w-7 h-1 transition-all duration-[600ms] before:content-[''] after:content-[''] before:bg-[#000000] after:bg-[#000000] before:w-7 after:w-7 before:h-1 after:h-1 before:transition-all after:transition-all before:duration-[600ms] after:duration-[600ms] before:absolute after:absolute before:rotate-45 after:-rotate-45"
-            )}
-          ></div>
-        </div>
-
-        {/* links */}
-        <ul className="flex flex-col gap-8 md:gap-16 text-secondary/white font-bold">
-          <li>
-            <a
-              href="#hero"
-              className=" text-white-100 md:text-3xl"
-              onClick={() => onOpenCloseClick()}
-            >
-              HOW IT WORKS
-            </a>
-          </li>
-          <li>
-            <a
-              href="#about"
-              className=" text-white-100 md:text-3xl"
-              onClick={() => onOpenCloseClick()}
-            >
-              ABOUT
-            </a>
-          </li>
-          <li>
-            <a
-              href="#work"
-              className=" text-white-100 md:text-3xl"
-              onClick={() => onOpenCloseClick()}
-            >
-              WORK
-            </a>
-          </li>
-          <li>
-            <a
-              href="#pricing"
-              className=" text-white-100 md:text-3xl"
-              onClick={() => onOpenCloseClick()}
-            >
-              PRICING
-            </a>
-          </li>
-          <li>
-            <a
-              href="#blog"
-              className=" text-white-100 md:text-3xl"
-              onClick={() => onOpenCloseClick()}
-            >
-              BLOG
-            </a>
-          </li>
-          <li>
-            <a
-              href="#faq"
-              className=" text-white-100 md:text-3xl"
-              onClick={() => onOpenCloseClick()}
-            >
-              FAQ
-            </a>
-          </li>
-          <li>
-            <Button className="font-bold">Get the Inito kit</Button>
-          </li>
-        </ul>
-      </div>
     </>
   );
 };
